20a6faee1fda33a6351f575395ec065319c931b4
Johan Holmberg <johan@idioti.se>
2022-04-22 18:23:00 +0200

    Incorporated SVG changes and added examples

--- dist/leaflet.awesome-svg-markers.js
+++ dist/leaflet.awesome-svg-markers.js
@@ -1,5 +1,5 @@
 /*
-  Leaflet.AwesomeMarkers, a plugin that adds colorful iconic markers for Leaflet, based on the Font Awesome icons
+  Leaflet.AwesomeSVGMarkers, a plugin that adds colorful iconic markers for Leaflet, based on the Font Awesome icons
   (c) 2012-2013, Lennard Voogdt
 
   http://leafletjs.com
@@ -11,14 +11,14 @@
 (function (window, document, undefined) {
     "use strict";
     /*
-     * Leaflet.AwesomeMarkers assumes that you have already included the Leaflet library.
+     * Leaflet.AwesomeSVGMarkers assumes that you have already included the Leaflet library.
      */
 
-    L.AwesomeMarkers = {};
+    L.AwesomeSVGMarkers = {};
 
-    L.AwesomeMarkers.version = '2.0.1';
+    L.AwesomeSVGMarkers.version = '1.0.0';
 
-    L.AwesomeMarkers.Icon = L.Icon.extend({
+    L.AwesomeSVGMarkers.Icon = L.Icon.extend({
         options: {
             iconSize: [35, 45],
             iconAnchor:   [17, 42],
@@ -39,10 +39,12 @@
         },
 
         createIcon: function () {
-            var div = document.createElement('div'),
+            const div = document.createElement('div'),
                 options = this.options;
 
-            if (options.icon) {
+            if (options.svgIcon) {
+                div.innerHTML = this._appendSVG(options);
+            } else if (options.icon) {
                 div.innerHTML = this._createInner();
             }
 
@@ -55,8 +57,28 @@
             return div;
         },
 
+        _appendSVG: function (options) {
+            let icon = options.svgIcon;
+            let iconColorStyle = '';
+            const classes = options.extraClasses;
+
+            if (icon.substring(0, 4) == 'data') {
+                icon = icon.slice(icon.indexOf(',') + 1);
+            }
+
+            if (options.iconColor) {
+                if(options.iconColor === 'white' || options.iconColor === 'black') {
+                    classes += " icon-" + options.iconColor;
+                } else {
+                    iconColorStyle = "style='fill: " + options.iconColor + "' ";
+                }
+            }
+
+            return '<span ' + iconColorStyle + ' class="' + classes + '">' + icon + '</span>';
+        },
+
         _createInner: function() {
-            var iconClass, iconSpinClass = "", iconColorClass = "", iconColorStyle = "", options = this.options;
+            let iconClass, iconSpinClass = "", iconColorClass = "", iconColorStyle = "", options = this.options;
 
             if(options.icon.slice(0,options.prefix.length+1) === options.prefix + "-") {
                 iconClass = options.icon;
@@ -80,7 +102,7 @@
         },
 
         _setIconStyles: function (img, name) {
-            var options = this.options,
+            let options = this.options,
                 size = L.point(options[name === 'shadow' ? 'shadowSize' : 'iconSize']),
                 anchor;
 
@@ -108,15 +130,15 @@
         },
 
         createShadow: function () {
-            var div = document.createElement('div');
+            let div = document.createElement('div');
 
             this._setIconStyles(div, 'shadow');
             return div;
       }
     });
         
-    L.AwesomeMarkers.icon = function (options) {
-        return new L.AwesomeMarkers.Icon(options);
+    L.AwesomeSVGMarkers.icon = function (options) {
+        return new L.AwesomeSVGMarkers.Icon(options);
     };
 
 }(this, document));
